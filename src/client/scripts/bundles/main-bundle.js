cfb557370a18cf2f4afb43bc02b5679c09235bb9
Daniel Moffat <danmofo@gmail.com>
2016-06-14 23:07:54 +0100

    Initial build of a new design idea, incorporating more interesting features

--- src/client/scripts/bundles/main-bundle.js
+++ src/client/scripts/bundles/main-bundle.js
@@ -3,7 +3,23 @@
 
   'use strict';
 
-  console.log('Hello world!');
+  var sun = document.querySelector('.scene__sky');
+  var header = document.querySelector('.website-header');
+
+  setTimeout(function() {
+
+    sun.classList.add('animation-moveInFromTop');
+
+  }, 500);
+
+
+  setTimeout(function() {
+
+    header.classList.add('animation-moveInFromTop');
+
+  }, 1000);
+
+  // var viewportWidth = document.documentElement.clientWidth;
 
 })();
 
