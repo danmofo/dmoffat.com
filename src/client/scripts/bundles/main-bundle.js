ac0aadbe0d10a624be36ddf10a1647cb7de63f56
Daniel Moffat <danmofo@gmail.com>
2016-07-06 19:22:53 +0100

    wip 6th july

--- src/client/scripts/bundles/main-bundle.js
+++ src/client/scripts/bundles/main-bundle.js
@@ -3,23 +3,11 @@
 
   'use strict';
 
-  var sun = document.querySelector('.scene__sky');
   var header = document.querySelector('.website-header');
 
   setTimeout(function() {
-
-    sun.classList.add('animation-moveInFromTop');
-
-  }, 500);
-
-
-  setTimeout(function() {
-
     header.classList.add('animation-moveInFromTop');
-
   }, 1000);
-
-  // var viewportWidth = document.documentElement.clientWidth;
 
 })();
 
--- src/client/scripts/main.js
+++ src/client/scripts/main.js
@@ -2,22 +2,10 @@
 
   'use strict';
 
-  var sun = document.querySelector('.scene__sky');
   var header = document.querySelector('.website-header');
 
   setTimeout(function() {
-
-    sun.classList.add('animation-moveInFromTop');
-
-  }, 500);
-
-
-  setTimeout(function() {
-
     header.classList.add('animation-moveInFromTop');
-
   }, 1000);
-
-  // var viewportWidth = document.documentElement.clientWidth;
 
 })();
