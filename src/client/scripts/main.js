1e9be86e28d358f32a94341906ff17460f78f387
Daniel Moffat <danmofo@gmail.com>
2017-09-06 23:24:55 +0100

    Finished styling the mobile bands.

--- src/client/scripts/main.js
+++ src/client/scripts/main.js
@@ -2,6 +2,8 @@
 
   'use strict';
 
+  var debounce = require('lodash.debounce');
+
   function $(selector) {
     return document.querySelector(selector);
   }
@@ -10,6 +12,23 @@
   var mobileNavigationContent = $('.js-navigation-content');
 
   mobileNavigationToggle.addEventListener('click', onToggleMobileNavigation);
+
+  var header = $('.js-header');
+
+  window.addEventListener('scroll', debounce(function(event) {
+
+    if(screenIsTablet() || document.body.scrollTop > 0) {
+        header.classList.add('is-visible');
+    } else {
+        header.classList.remove('is-visible');
+    }
+
+
+  }, 200));
+
+  function screenIsTablet() {
+    return window.innerWidth >= 480;
+  }
 
   function onToggleMobileNavigation(event) {
     mobileNavigationContent.classList.toggle('is-active');
